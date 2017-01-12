a72ca42d78c3296391e8250f2c711f9dc947abd7
TONY-PC\Tony <skitty614@gmail.com>
2017-01-11 22:03:37 -0600

    BT11 -> Virtual and Overrides.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -4,9 +4,24 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller posessing: %s"), *ControlledTank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
+	
 	return Cast<ATank>(GetPawn());
 }
 
